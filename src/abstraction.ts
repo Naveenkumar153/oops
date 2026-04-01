09b21bcd6dc4b1a612d9d4aa0c6b67411b579dd8
Naveenkumar153 <naveenkumartech2025@gmail.com>
2026-04-01 23:52:28 +0530

    feat: implement payment processing with CreditCardPayment and UpiPayment classes

---
+++ src/abstraction.ts
@@ -0,0 +1,41 @@
+interface PaymentMethod {
+    pay(): void;
+}
+
+abstract class Payment {
+    constructor(protected amount: number) {}
+
+    abstract pay(): void;
+
+    printReceipt(): void {
+        console.log(`Payment of $${this.amount} has been processed.`);
+    }
+};
+
+export class CreditCardPayment extends Payment implements PaymentMethod {
+    constructor(amount: number, private cardNumber: string) {
+        super(amount);
+    }
+
+    pay(): void {
+        console.log(`
+            Processing credit card payment of $${this.amount} 
+            using card number ${this.cardNumber}.`
+        );
+        this.printReceipt();
+    }
+};
+
+export class UpiPayment extends Payment implements PaymentMethod {
+    constructor(amount: number, private upiId: string) {
+        super(amount);
+    };
+
+    pay(): void {
+        console.log(` 
+            Processing UPI payment of $${this.amount} 
+            using UPI ID ${this.upiId}.`
+        );
+        this.printReceipt();
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import { Person } from "./encapsulation.js";
+import { CreditCardPayment, UpiPayment } from "./abstraction.js";
 
 const person = new Person("Naveen", 30, true, 'Engineer');
 console.log(person.getName); // Output: Naveen
 console.log(person.getAge);  // Output: 30
 console.log(person.getIsMarried);  // Output: true
 console.log(person.getOccupation);  // Output: Engineer
+
+const Payment = new CreditCardPayment(100, "1234-5678-9012-3456");
+Payment.pay();
+// Output:
+// Processing credit card payment of $100 
+// using card number 1234-5678-9012-3456.
+// Payment of $100 has been processed.
+
+const upiPayment = new UpiPayment(50, "musk@upi");
+upiPayment.pay();
+// Output:
+// Processing UPI payment of $50 
+// using UPI ID musk@upi.
+// Payment of $50 has been processed.
